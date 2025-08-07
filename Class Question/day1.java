b384fa7e2f6d4f64b1453b61f6c94addaed39f7e
raushan <raushansahu147@gmail.com>
2025-08-07 14:11:10 +0530

    d

--- Class Question/day1.java
+++ Class Question/day1.java
@@ -4,11 +4,11 @@ public class day1 {
 
         int arr[] ={2,2,4,5,5,5,6,7,99};
 
-        
+
         int count =1;
         int currect_unique=arr[0];
         for(int i=1; i<arr.length; i++){
-            if(arr[i] != currect_unique){
+            if(arr[i] != currect_unique ){
                 count++;
                 currect_unique=arr[i];
             }
